a92c981cf3cc1142c26ae34fbebed3d74e1fdddc
Frany6710 <61435815+Frany6710@users.noreply.github.com>
2021-02-20 00:37:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require("express")
 const bodyp = require("body-parser")
+const { Webhook } = require("discord-webhook-node")
+const mail = require("nodemailer")
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+
+var trans = mailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "automaticmail.frany6710@gmail.com",
+        pass: "gatonto18"
+    }
+})
+
 
 app.post("/send", (req, res)=>{
     let mail = req.body.email
     let name = req.body.name
     console.log(req.body)
     res.send(`Nombre: ${name} \nEmail: ${mail}`)
+    var mailOptions = {
+    from: "Sistema de mails automaticos de frany6710 <automaticmail.frany6710@gmail.com>",
+    to: mail,
+    subject: "Espereme",
+    text: "Frany lo contactará en un plazo de 24 horas"
+        
+}
+    trans.sendMail(mailOptions, (err, info) => {}
+    if (err) {
+        return console.log(err)
+    }
+    const hook = new Webhook("https://discord.com/api/webhooks/812137564097085492/RmVQqyCNwbEN8Uvv2itlz3eFb7lK6gVA50L1ZregtPXKKLHYDkabhHVc0ko3WyECqBqL")
+    hook.setUsername("Frany6710 Mails")
+    hook.setAvatar("./img.png")
+    hook.send("Mail: "+mail+"\nNombre: "+name)
+})
+    }
+
+
 })
 
 app.listen(process.env.PORT || 3000, ()=>{
