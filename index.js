47ad08b0d33fbbfb02439161e556eaae7d8cce93
Frany6710 <61435815+Frany6710@users.noreply.github.com>
2021-02-19 22:51:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.post("/send", urlencoderparser, (req, res)=>{
     res.redirect("https://github.com")
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.port || 3000, ()=>{
     console.log("Listening on port 3000")
 })
